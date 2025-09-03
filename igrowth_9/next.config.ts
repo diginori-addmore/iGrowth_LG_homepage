3a9bd05e6f3174136592f6ac41ff591f16d88fec
diginori-minkyu <jademin2033@diginori.com>
2025-09-03 22:57:52 +0900

    next.config.ts 수정

--- igrowth_9/next.config.ts
+++ igrowth_9/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,  // App Router 사용
+  },
+  // src/app/ 사용 명시
+  // src를 루트처럼 취급하도록 설정
+  output: 'standalone',
 };
 
 export default nextConfig;
