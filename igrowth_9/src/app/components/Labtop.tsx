48e96ae1af5d6ed76b696b6a959d276c132f5bfe
stundrg <khr0106@naver.com>
2025-09-04 15:25:49 +0900

    노트북 위치 조정

--- igrowth_9/src/app/components/Labtop.tsx
+++ igrowth_9/src/app/components/Labtop.tsx
@@ -1,20 +1,20 @@
 export default function Labtop() {
     return (
-        <section className="relative flex items-center justify-center bg-gradient-to-b from-white to-pink-200 py-16 px-8 overflow-hidden gap-x-6">
+        <section className="relative flex items-center justify-center bg-gradient-to-b from-white to-pink-200 pt-0 pb-16 px-8 overflow-hidden gap-x-0 -mt-2">
 
             {/* 왼쪽 이미지 */}
-            <div className="flex-1 flex justify-center items-end overflow-hidden">
+            <div className="flex-1 flex justify-end items-end overflow-hidden pr-32">
                 {/* TODO: 이미지 교체 */}
                 <img
                     src="/labtop.png"
                     alt="랩탑"
-                    className="h-56 object-contain"
+                    className="h-48 object-contain"
                 // translate-y-6
                 />
             </div>
 
             {/* 오른쪽 텍스트 */}
-            <div className="flex-1 flex flex-col justify-center items-start text-left px-4">
+            <div className="flex-1 flex flex-col justify-center items-start text-left pl-0">
                 <h2 className="text-4xl font-bold text-gray-900 mb-4">
                     교육용 <span className="text-pink-500">고성능 노트북 무료 </span>지원
                 </h2>
