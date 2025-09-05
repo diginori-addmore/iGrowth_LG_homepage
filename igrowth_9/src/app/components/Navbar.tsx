e9edf80851a9ce9c6cf1c37c420c087874ea8c55
jiwon1118 <b23386585@gmail.com>
2025-09-05 15:01:53 +0900

    logo

--- igrowth_9/src/app/components/Navbar.tsx
+++ igrowth_9/src/app/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function Navbar() {
     return (
         <nav className="flex z-20 justify-between items-center px-6 py-4 bg-white shadow-md fixed w-full z-10">
-            <div className="text-2xl font-bold text-pink-500">LG U+</div>
+            {/* <Image src="/logo.png" alt="LG U+" width={100} height={40} /> */}
+            <div className="flex items-center gap-2">
+                <Image src="/logo.png" alt="LG U+" width={100} height={40} />
+                <span className="text-base font-bold text-black-500">Why Not SW Camp</span>
+            </div>
             <ul className="hidden md:flex gap-6 text-gray-700">
                 <li><a href="#features">특징</a></li>
                 <li><a href="#curriculum">커리큘럼</a></li>
