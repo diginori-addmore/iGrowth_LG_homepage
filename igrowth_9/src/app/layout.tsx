39898995edb50473fb5621450f52de6213bf8ec4
diginori-minkyu <jademin2033@diginori.com>
2025-09-08 11:29:59 +0900

    dark모드 무시

--- igrowth_9/src/app/layout.tsx
+++ igrowth_9/src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="ko">
+        <html lang="ko" className='dark'>
             <body className="font-sans bg-white text-gray-900">
                 {children}
             </body>
