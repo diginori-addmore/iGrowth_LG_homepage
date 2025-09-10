6eecb0e466eeb922ef0711e076e43649afea1a3f
diginori-jaeyoung <jacob@diginori.com>
2025-09-10 11:01:53 +0900

    favicon 추가

--- igrowth_9/src/app/layout.tsx
+++ igrowth_9/src/app/layout.tsx
@@ -9,6 +9,7 @@ import { Suspense } from 'react' // ⬅️ 추가
 export const metadata: Metadata = {
   title: 'LG U+ 연계 | Why Not SW Camp',
   description: '국내 유일 AI 경력 증명서 발급 부트캠프! 교육과 동시에 실무 이력 만들고 200% 취업하기',
+  icons:{icon:"/favicon.png"},
   openGraph: {
     title: '국내 유일 AI 경력 증명서 발급 부트캠프',
     description: 'IT 교육의 새로운 패러다임, 100% 실무 이력 만들기',
